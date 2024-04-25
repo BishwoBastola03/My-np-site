a03a5380dc0c30cf2e2f3a7b9e09195229461b56
BishwoBastola03 <161615824+BishwoBastola03@users.noreply.github.com>
2024-04-25 16:52:04 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+  setTimeout(function() {
+    window.location.href = 'mainPage.html'; // Redirect to your main content page after 5 seconds
+  }, 5000);
+});
